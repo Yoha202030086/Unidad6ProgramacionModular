e9600f0f1aaf85eb66ad860a6921eb8d0fc7449e
MaríaPérez <yohanalpz17@gmail.com>
2021-06-13 16:16:58 -0600

    diez

---
+++ src/diez.java
@@ -0,0 +1,30 @@
+package src;
+
+public class diez {
+
+    public static void main(String[] args) 
+    {
+        diez d = new diez(); 
+    }
+    
+
+
+     public diez(){
+     int [] [] a = { { 5, 2, 18, 9 }, { 4, 3, 15, 7 }, { 1,6,8,0 } }; //matriz de 4x3 
+     int [] transpuesta = new int[4,3];
+     int m, n;
+      
+     for(m = 0; m < 3; m++){
+         for(n = 0; n < 4; n++){
+
+                transpuesta[m,n] = a[m,n];
+           }
+         }
+     for(m = 0; m < 4; m++){
+        for(n = 0; n < 3; n++){
+
+Console.ReadKey();
+
+ }
+
+
